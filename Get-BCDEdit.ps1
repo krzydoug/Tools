0cf441367f380d1d51d5610295959f5c79b41af6
krzydoug <dmaurer80@yahoo.com>
2021-08-20 13:40:01 -0500

    Update Get-BCDEdit.ps1

--- Get-BCDEdit.ps1
+++ Get-BCDEdit.ps1
@@ -1,17 +1,31 @@
-bcdedit /V | where {$_ -match "^[^-]"}| 
-    foreach -Begin {$props = $null} -Process {
-        $a,$b = $_.Split(' ',2).trim()
-        if($a -eq 'Windows')
+#requires -RunAsAdministrator
+
+$ErrorActionPreference = 'Stop'
+
+try{
+    $result = bcdedit /V | where {$_ -match "^[^-|^ ]"}
+    
+    if(!$?){
+        Write-Warning "BCD command failed. Are you running as admin?"
+    }
+
+    $result | ForEach-Object -Begin {$props = $null} -Process {
+        $key,$value = $_.Split(' ',2).trim()
+        if($key -eq 'Windows')
         {
             if($props.keys.Count -gt 0)
             {
                 [pscustomobject]$props
             }
             $props = [ordered]@{}
-            $props.Add("BCD Entry",$b)
+            $props.Add("BCD Entry",$value)
         }
         else
         {
-            $props.Add($a,$b)
+            $props.Add($key,$value)
         }
     } -End {[pscustomobject]$props} -OutVariable bcd
+}
+catch{
+    Write-Warning "Error getting BCD info: $($result)"
+}
