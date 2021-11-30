7aede3ba51d0980501da1d492bc3158e1cad614e
krzydoug <dmaurer80@yahoo.com>
2021-11-29 18:46:58 -0600

    Update Get-FolderSize.ps1

--- Get-FolderSize.ps1
+++ Get-FolderSize.ps1
@@ -1,47 +1,56 @@
 Function Get-FolderSize {
     [cmdletbinding()]
     Param(
-        [Parameter(Mandatory,Position=0)]
+        [Parameter(Mandatory,Position=0,ValueFromPipelineByPropertyName)]
         [ValidateScript({
-            if(Test-Path $_){
+            if(Test-Path -LiteralPath $_){
                 $true
             }
             else{
                 Throw "Invalid path: $_"
             }
-            if((Get-Item $_).PSIsContainer){
+            if((Get-Item -LiteralPath $_).PSIsContainer){
                 $true
             }
             else{
                 Throw "$_ is not a directory."
             }
         })]
-        [String]$Path
+        [String]$FullName
     )
 
-    # Get the total directory number and file counts
-    # the '/L' [for List] tells robocopy to not do anything, just list what it _would_ do
-    #    /E :: copy subdirectories, including Empty ones.
-    #    /L :: List only - don't copy, timestamp or delete any files.
-    #    /NFL :: No File List - don't log file names.
-    #    /NDL :: No Directory List - don't log directory names.
-    #    /NP :: No Progress - don't display percentage copied.
-
-    $RC_Results = robocopy $Path 'NULL' /L /E /NP /NFL /NDL
-   
-    $dirs,$files,$size = switch -Regex ($RC_Results | Select-Object -Last 6){
-        '(Ended|Times)\s:\s(.+)' {break}
-        ':' {
-            (-split ($_  -replace '(?<=\d)\s(?=[bkmg])|(?<=\s):(?=\s)'))[1]
-        }
+    begin{
+        # Get the total directory number and file counts
+        # the '/L' [for List] tells robocopy to not do anything, just list what it _would_ do
+        #    /E :: copy subdirectories, including Empty ones.
+        #    /L :: List only - don't copy, timestamp or delete any files.
+        #    /NFL :: No File List - don't log file names.
+        #    /NDL :: No Directory List - don't log directory names.
+        #    /NP :: No Progress - don't display percentage copied.
     }
+
+    process {
+
+        $RC_Results = robocopy $FullName 'NULL' /L /E /NP /NFL /NDL
    
-    [PSCustomObject] @{
-        DirPath   = $Path
-        DirCount  = "{0}" -f $dirs
-        FileCount = "{0}" -f $files
-        TotalSize = "{0}" -f $(if($size -match '\w$'){
-            $size -replace '\w$',"$(($matches.0).toupper())B"
-        })
+        $dirs,$files,$size = switch -Regex ($RC_Results | Select-Object -Last 6){
+            '(Ended|Times)\s:\s(.+)' {break}
+            ':' {
+                (-split ($_  -replace '(?<=\d)\s(?=[bkmg])|(?<=\s):(?=\s)'))[1]
+            }
+        }
+        
+        [PSCustomObject] @{
+            DirCount  = "{0}" -f $dirs
+            FileCount = "{0}" -f $files
+            TotalSize = "{0:N2}" -f $(if($size -match '\D$'){
+                $formattedsize = $size -replace '\D$',"$($matches.0)B"
+                Invoke-Expression $formattedsize
+            }
+            else{
+                $size
+            }) -as [decimal]
+            DirPath   = $FullName
+        }
     }
 }
