4757cb85248225d2b47417180e058634a3e9da4d
krzydoug <dmaurer80@yahoo.com>
2021-12-01 19:28:29 -0600

    Update Get-ObjectClass.ps1

--- Get-ObjectClass.ps1
+++ Get-ObjectClass.ps1
@@ -1,23 +1,23 @@
-        <#
-        .Synopsis
-           List all ObjectClass attributes for ADUser
-        .DESCRIPTION
-           List all ObjectClass attributes for ADUser
-        .EXAMPLE
-           Get-ObjectClass "Username"
-        .EXAMPLE
-           Get-ObjectClass CN=Username,OU=Users,DC=Domain,DC=LOCAL
-        .EXAMPLE
-           get-aduser -filter * | Get-ObjectClass
-        .INPUTS
-           [String[]] (SamAccountName, SID, DistinguishedName)
-        .OUTPUTS
-           [string[]]
-        .NOTES
-           Powershell cmdlets get-adobject and get-aduser only return the objectclass of which the user is an instance. It does not include auxillary classes.
-        #>
-function Get-ObjectClass
-{
+function Get-ObjectClass{
+    <#
+    .Synopsis
+        List all ObjectClass attributes for ADUser
+    .DESCRIPTION
+        List all ObjectClass attributes for ADUser
+    .EXAMPLE
+        Get-ObjectClass "Username"
+    .EXAMPLE
+        Get-ObjectClass CN=Username,OU=Users,DC=Domain,DC=LOCAL
+    .EXAMPLE
+        get-aduser -filter * | Get-ObjectClass
+    .INPUTS
+        [String[]] (Name, UserPrincipalName, SamAccountName, SID, DistinguishedName)
+    .OUTPUTS
+        [string[]]
+    .NOTES
+        Powershell cmdlets get-adobject and get-aduser only return the objectclass of which the user is an instance. It does not include auxillary classes.
+    #>
+
     [CmdletBinding()]
     [Alias("goc")]
     [OutputType([String[]])]
@@ -35,29 +35,34 @@ function Get-ObjectClass
         
     )
         
-    Begin
-    {
+    Begin{
         $ErrorActionPreference = 'Stop'
     }
 
-    Process
-    {
-        foreach($id in $Identity)
-        {
-            try
-            {
-                $user = get-aduser $Id
+    Process{
+
+        foreach($id in $Identity){
+
+            $filter = switch -Regex ($currentuser){
+                '=' {'DistinguishedName';break}
+                '@' {'UserPrincipalName';break}
+                ' ' {'Name';break}
+                'S-\d' {'SID';break}
+                default {'SamAccountName'}
+            }
+
+            try{
+                $user = Get-ADUser -Filter "$filter -eq '$id'"
             }
             catch{
-                Write-Warning "Unable to find ADUser $Id"
+                Write-Warning "Unable to find AD user $id"
                 break
             }
+
             (dsquery * $user.distinguishedname -scope base -attr objectclass |
                 select -Skip 1).trim() -split ';' | where {$_ -ne ''}
         }
     }
 
-    End
-    {
-    }
+    End{}
 }
