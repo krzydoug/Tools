153909ada225c5bf75f61663ab065a1f40306b01
krzydoug <dmaurer80@yahoo.com>
2021-12-01 19:13:42 -0600

    Create Get-Netstat.ps1

---
+++ Get-Netstat.ps1
@@ -0,0 +1,32 @@
+Function Get-Netstat {
+
+    # https://www.reddit.com/r/PowerShell/comments/khiwjo/parsing_netstat_ano/
+
+    $sb = {
+        param($proto,$pcid,$state)
+        $localaddr,$localport,$remoteaddr,$remoteport = $_[1..2] |
+            Foreach-Object {-split ($_ -replace '(^.+):(.+$)','$1 $2')}
+        [PSCustomObject]@{
+            Protocol      = $proto
+            LocalAddress  = $localaddr
+            LocalPort     = $localport
+            RemoteAddress = $remoteaddr
+            RemotePort    = $remoteport
+            ProcessID     = $pcid
+            State         = $state
+        }
+    }
+
+    switch -Regex (netstat -ano){
+        'TCP' {
+            , -split $_ | ForEach-Object {
+                $sb.Invoke($($_[0,4,3]))
+            }
+        }
+        'UDP' {
+            , -split $_ | ForEach-Object {
+                $sb.Invoke($_[0],$_[3],'Stateless')
+            }
+        }
+    }
+}
