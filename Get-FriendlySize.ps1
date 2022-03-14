a7b460dbda2b2d9f28a5c7c975fe514934da6bce
krzydoug <dmaurer80@yahoo.com>
2022-03-13 23:45:43 -0500

    Create Get-FriendlySize.ps1

---
+++ Get-FriendlySize.ps1
@@ -0,0 +1,13 @@
+Function Get-FriendlySize {
+    [cmdletbinding()]
+    Param([bigint]$bytes)
+    
+    switch($bytes){
+        {$_ -gt 1PB}{"{0:N2} PB" -f ($_ / 1PB);break}
+        {$_ -gt 1TB}{"{0:N2} TB" -f ($_ / 1TB);break}
+        {$_ -gt 1GB}{"{0:N2} GB" -f ($_ / 1GB);break}
+        {$_ -gt 1MB}{"{0:N2} MB" -f ($_ / 1MB);break}
+        {$_ -gt 1KB}{"{0:N2} KB" -f ($_ / 1KB);break}
+        default {"{0:N2} Bytes" -f $_}
+    }
+}
