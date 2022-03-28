37dd29b55dc30493bb850f61fb02354a5c23bc8a
krzydoug <dmaurer80@yahoo.com>
2022-03-28 14:47:09 -0500

    Update Get-SslCertificate.ps1

--- Get-SslCertificate.ps1
+++ Get-SslCertificate.ps1
@@ -13,6 +13,25 @@ function Get-SslCertificate {
         ValidFrom  : 7/28/2021 4:22:06 PM
         Expiration : 7/28/2022 4:22:06 PM
         Thumbprint : B5BC7B1FD96BE16E49CB61354824CF4259A2BE75
+    .EXAMPLE
+        PS C:\> 'msn.com','google.com'|Get-SslCertificate
+
+        Url        : https://msn.com
+        Name       : CN=*.msn.com
+        Subject    : CN=*.msn.com
+        Issuer     : C=US, O=Microsoft Corporation, CN=Microsoft RSA TLS CA 01
+        ValidFrom  : 9/22/2021 3:59:56 PM
+        Expiration : 9/22/2022 3:59:56 PM
+        Thumbprint : 1EA7EFDB1B73318C2FB75B9A57D647894263070A
+
+        WARNING: The certificate for https://google.com expires in 72 days
+        Url        : https://google.com
+        Name       : CN=www.google.com
+        Subject    : CN=www.google.com
+        Issuer     : C=US, O=Google Trust Services LLC, CN=GTS CA 1C3
+        ValidFrom  : 3/17/2022 6:49:13 AM
+        Expiration : 6/9/2022 6:49:12 AM
+        Thumbprint : FA202003DD8DD881FC80284BFE33343DD8D61788
     .INPUTS
         String
     .OUTPUTS
