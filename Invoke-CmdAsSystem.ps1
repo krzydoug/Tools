7089f77d951af91b54354f7911d4e8533c254091
krzydoug <dmaurer80@yahoo.com>
2022-07-02 20:40:19 -0500

    Check for elevation
    psexec requires admin to run as system.

--- Invoke-CmdAsSystem.ps1
+++ Invoke-CmdAsSystem.ps1
@@ -1,13 +1,29 @@
 Function Invoke-CmdAsSystem {
+    [CmdletBinding()]
+    param (
+        [parameter(position=0)]
+        [Validateset('Desktop','Core')]
+        [validatescript({if($_ -eq 'core' -and -not (Get-ChildItem HKLM:\Software\Microsoft\PowerShellCore\InstalledVersions |Where-Object{(Get-ItemProperty $_.pspath) -match '(6|7)\.\d+\.\d+'})){throw "Powershell core is not detected"}else{$true}})]
+        $Edition = 'Desktop'
+    )
 
-    if(-not (Test-Path c:\temp)){
-        $null = New-Item c:\Temp -ItemType Directory
+    Write-Verbose "Verifying process is elevated"
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    $iselevated = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+    if($iselevated -ne $true){
+        Write-Warning "Invoke-CmdAsSystem must be ran as administrator"
+        return
     }
 
-    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+    $psexec = Join-Path $env:TEMP 'Psexec.exe'
+    
+    if(-not (Test-Path $psexec)){
+        Write-Verbose "Downloading psexec.exe from live.sysinternals.com"
+        [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+        Invoke-WebRequest https://live.sysinternals.com/tools/psexec.exe -OutFile $psexec -UseBasicParsing
+    }
 
-    Invoke-WebRequest https://live.sysinternals.com/tools/psexec.exe -OutFile c:\temp\psexec.exe
-
-    C:\temp\psexec.exe /s /i cmd.exe /accepteula
+    Start-Process $psexec -ArgumentList '/s','/i','cmd.exe','/accepteula','/nobanner' -WindowStyle Hidden 2>&1
 
 }
--- Invoke-PowershellAsSystem.ps1
+++ Invoke-PowershellAsSystem.ps1
@@ -6,23 +6,29 @@ function Invoke-PowershellAsSystem {
         [validatescript({if($_ -eq 'core' -and -not (Get-ChildItem HKLM:\Software\Microsoft\PowerShellCore\InstalledVersions |Where-Object{(Get-ItemProperty $_.pspath) -match '(6|7)\.\d+\.\d+'})){throw "Powershell core is not detected"}else{$true}})]
         $Edition = 'Desktop'
     )
- 
-    $psexec = 'C:\Temp\Psexec.exe'
+
+    Write-Verbose "Verifying process is elevated"
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    $iselevated = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+    if($iselevated -ne $true){
+        Write-Warning "Invoke-PowershellAsSystem must be ran as administrator"
+        return
+    }
+
+    $psexec = Join-Path $env:TEMP 'Psexec.exe'
 
     $exe = @{
         Desktop = 'Powershell.exe'
         Core    = 'Pwsh.exe'
     }
 
-    if(-not (Test-Path c:\temp)){
-        $null = New-Item c:\Temp -ItemType Directory
-    }
-
     if(-not (Test-Path $psexec)){
+        Write-Verbose "Downloading psexec.exe from live.sysinternals.com"
         [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-        Invoke-WebRequest https://live.sysinternals.com/tools/psexec.exe -OutFile c:\temp\psexec.exe
+        Invoke-WebRequest https://live.sysinternals.com/tools/psexec.exe -OutFile $psexec -UseBasicParsing
     }
 
-    $null = cmd /c "$psexec /s /i $($exe[$Edition]) /accepteula /nobanner" 2>&1
+    Start-Process $psexec -ArgumentList '/s','/i',$exe[$Edition],'/accepteula','/nobanner' -WindowStyle Hidden 2>&1
 
 }
