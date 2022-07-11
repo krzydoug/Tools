c5b70c789540f893fbbe53ce8cd14e498de7d0b3
Doug Maurer <dmaurer80@yahoo.com>
2022-07-11 16:37:51 -0500

    Update Get-Netstat.ps1

--- Get-Netstat.ps1
+++ Get-Netstat.ps1
@@ -1,7 +1,9 @@
 Function Get-Netstat {
     [cmdletbinding()]
     [outputtype([PSCustomObject[]])]
-    Param()
+    Param(
+        [switch]$IncludeProcessDetails
+    )
     
     # https://www.reddit.com/r/PowerShell/comments/khiwjo/parsing_netstat_ano/
 
@@ -9,6 +11,7 @@ Function Get-Netstat {
         param($proto,$pcid,$state)
         $localaddr,$localport,$remoteaddr,$remoteport = $_[1..2] |
             Foreach-Object {-split ($_ -replace '(^.+):(.+$)','$1 $2')}
+            
         [PSCustomObject]@{
             Protocol      = $proto
             LocalAddress  = $localaddr
@@ -20,7 +23,7 @@ Function Get-Netstat {
         }
     }
 
-    switch -Regex (netstat -ano){
+    $output = switch -Regex (netstat -ano){
         'TCP' {
             , -split $_ | ForEach-Object {
                 $sb.Invoke($($_[0,4,3]))
@@ -31,5 +34,23 @@ Function Get-Netstat {
                 $sb.Invoke($_[0],$_[3],'STATELESS')
             }
         }
+    }
+
+    if($IncludeProcessDetails){
+        $process = Get-CimInstance -ClassName Win32_Process | Group-Object -Property ProcessID -AsHashTable -AsString
+
+        $output | ForEach-Object {
+            $owner = $process[$_.processid] | Invoke-CimMethod -MethodName GetOwner |ForEach-Object{
+                if($_.returnvalue -eq 0){$_.Domain,$_.User -join '\'}
+            }
+
+            $_ | Select-Object *,@{n='FilePath';e={$process.$($_.processid).Path}},
+                                 @{n='Owner';e={$owner}},
+                                 @{n='StartTime';e={$process.$($_.processid).CreationDate}},
+                                 @{n='CommandLine';e={$process.$($_.processid).CommandLine}}
+        }
+    }
+    else{
+        $output
     }
 }
