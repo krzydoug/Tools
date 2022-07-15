09c4feb01ee13a24dbde18acafabe22d27aebaf4
Doug Maurer <dmaurer80@yahoo.com>
2022-07-15 17:51:29 -0500

    Update Invoke-RemoteExpression.ps1

--- Invoke-RemoteExpression.ps1
+++ Invoke-RemoteExpression.ps1
@@ -9,158 +9,341 @@ Function Invoke-RemoteExpression {
     ##############################################################################
 
     <#
-
     .SYNOPSIS
+        Invoke a PowerShell expression on a remote machine. Requires PsExec from
+        http://live.sysinternals.com/tools/psexec.exe. If the remote machine
+        supports PowerShell version two, use PowerShell remoting instead.
 
-    Invoke a PowerShell expression on a remote machine. Requires PsExec from
-    http://live.sysinternals.com/tools/psexec.exe. If the remote machine
-    supports PowerShell version two, use PowerShell remoting instead.
+    .DESCRIPTION
+        A major overhaul of a classic script written by the brilliant Lee Holmes. Added support for commands longer than 8190
+        characters, multithreading, and support for running as System. Powershell remoting is almost always the way to go, but there
+        are some times where it may not be available or working. The number one use case I've found is when a computers name exceeds
+        15 characters. Next most common is occassional environment where someone made the poor decision to not enable PS Remoting.
+    
+    .PARAMETER ComputerName
+        One or more computer names which can also be provided as pipeline input.
+    
+    .PARAMETER ScriptBlock
+        The script/command to execute remotely. This can be a string or a scriptblock. If hidden admin share c$ is available, then any
+        size script can be used. 
+    
+    .PARAMETER Credential
+        The credential to be used for the remote execution. This can be a simple username (you will be prompted for password) or a 
+        credential object.
+    
+    .PARAMETER NoProfile
+        Specifies not to load the powershell profile on the remote system.
+    
+    .PARAMETER System
+        Specifies to run powershell as 'NT Authority\System' on the remote system.
+    
+    .PARAMETER ThrottleLimit
+        Specifies the maximum number of concurrent threads allowed. Defaults to 32.
 
     .EXAMPLE
-
-    PS > Invoke-RemoteExpression LEE-DESK { Get-Process }
-    Retrieves the output of a simple command from a remote machine
-
-    .EXAMPLE
-
-    PS > (Invoke-RemoteExpression LEE-DESK { Get-Date }).AddDays(1)
-    Invokes a command on a remote machine. Since the command returns one of
-    PowerShell's primitive types (a DateTime object,) you can manipulate
-    its output as an object afterward.
+        Invoke-RemoteExpression LEE-DESK 'Hostname;ipconfig /all'
+        Retrieves the output of simple commands from a remote machine
 
     .EXAMPLE
+        (Invoke-RemoteExpression LEE-DESK { Get-Date }).AddDays(1)
+        Invokes a command on a remote machine. Since the command returns one of
+        PowerShell's primitive types (a DateTime object,) you can manipulate
+        its output as an object afterward.
 
-    PS > Invoke-RemoteExpression LEE-DESK { Get-Process } | Sort Handles
-    Invokes a command on a remote machine. The command does not return one of
-    PowerShell's primitive types, but you can still use PowerShell's filtering
-    cmdlets to work with its structured output.
-
+    .EXAMPLE
+        Invoke-RemoteExpression LEE-DESK { Get-Process } | Sort Handles
+        Invokes a command on a remote machine. The command does not return one of
+        PowerShell's primitive types, but you can still use PowerShell's filtering
+        cmdlets to work with its structured output.
+    
+    .EXAMPLE
+        $ComputerList | Invoke-RemoteExpression -ScriptBlock $ScriptBlock -Verbose
+        Executes the defined scriptblock against the list of computers. This can be
+        a simple string array of computer names or an object with ComputerName property.
+    
+    .LINK
+        https://github.com/krzydoug/Tools/blob/master/Invoke-RemoteExpression.ps1
+    
+        
     #>
 
     param(
-        ## The computer on which to invoke the command.
-        $ComputerName = "$ENV:ComputerName",
+        [Parameter(Mandatory,Position = 0,ValueFromPipeline,ValueFromPipelineByPropertyName,HelpMessage='The computer(s) on which to invoke the command')]
+        [string[]]$ComputerName,
 
-        ## The scriptblock to invoke on the remote machine.
-        [Parameter(Mandatory = $true)]
-        [ScriptBlock] $ScriptBlock,
+        [Parameter(Mandatory,Position = 1,HelpMessage='The scriptblock to invoke on the remote machine(s)')]
+        [string]$ScriptBlock,
 
-        ## The username / password to use in the connection
-        $Credential,
+        [Parameter(HelpMessage='The username/password to use for the remote connection')]
+        [Alias('PSCredential')] 
+        [ValidateNotNull()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]$Credential,
 
-        ## Determines if PowerShell should load the user's PowerShell profile
-        ## when invoking the command.
-        [switch] $NoProfile
+        [Parameter(HelpMessage='Determines if powershell should load the user profile on the remote machine')]
+        [switch]$NoProfile,
+
+        [Parameter(HelpMessage='Determines if powershell should be executed as the system account on the remote machine')]
+        [switch]$System,
+
+        [Parameter(HelpMessage='Determines the maximum number of threads that can run concurrently')]
+        [int]$ThrottleLimit = 32
     )
 
-    Set-StrictMode -Version 3
-    $file = "PsExec.exe"
-    $path = $env:TEMP
-    $psexec = Join-Path -Path $path -ChildPath $file
-    Function Download-PsExec{
-        #Sources:
-        # blog.jourdant.me/3-ways-to-download-files-with-powershell/
-        # blogs.technet.microsoft.com/heyscriptingguy/2011/06/17/manage-event-subscriptions-with-powershell/
+    begin{
+        #Set-StrictMode -Version 3
+        $ErrorActionPreference = "Stop"
+        $title = $host.UI.RawUI.WindowTitle
+        $file = "PsExec.exe"
+        $path = $env:TEMP
+        $psexec = Join-Path -Path $path -ChildPath $file
 
-        # global variables
-        $global:lastpercentage = -1
-        $global:are = New-Object System.Threading.AutoResetEvent $false
+        Function Download-PsExec{
+            #Sources:
+            # blog.jourdant.me/3-ways-to-download-files-with-powershell/
+            # blogs.technet.microsoft.com/heyscriptingguy/2011/06/17/manage-event-subscriptions-with-powershell/
 
-        # variables
-        $global:file = "PsExec.exe"
-        $global:path = $env:TEMP
-        $uri = "https://live.sysinternals.com/PsExec.exe"
-        $of = join-path -Path $env:temp -ChildPath "PsExec.exe"
+            # global variables
+            $global:lastpercentage = -1
+            $global:are = New-Object System.Threading.AutoResetEvent $false
 
-        # web client
-        # (!) output is buffered to disk -> great speed
-        $wc = New-Object System.Net.WebClient
+            # variables
+            $global:file = "PsExec.exe"
+            $global:path = $env:TEMP
+            $uri = "https://live.sysinternals.com/PsExec.exe"
+            $of = join-path -Path $env:temp -ChildPath "PsExec.exe"
 
-        Register-ObjectEvent -InputObject $wc -EventName DownloadProgressChanged -Action {
-            # (!) getting event args
-            $percentage = $event.sourceEventArgs.ProgressPercentage
-            if($global:lastpercentage -lt $percentage)
-            {
-                $global:lastpercentage = $percentage
-                # stackoverflow.com/questions/3896258
-                Write-progress -activity "Downloading PsExec" -PercentComplete $percentage -Status "Downloading $global:file to $global:path"
+            # web client
+            # (!) output is buffered to disk -> great speed
+            $wc = New-Object System.Net.WebClient
+
+            Register-ObjectEvent -InputObject $wc -EventName DownloadProgressChanged -Action {
+                # (!) getting event args
+                $percentage = $event.sourceEventArgs.ProgressPercentage
+                if($global:lastpercentage -lt $percentage)
+                {
+                    $global:lastpercentage = $percentage
+                    # stackoverflow.com/questions/3896258
+                    Write-progress -activity "Downloading PsExec" -PercentComplete $percentage -Status "Downloading $global:file to $global:path"
+                }
+            } > $null
+
+            Register-ObjectEvent -InputObject $wc -EventName DownloadFileCompleted -Action {
+                $global:are.Set()
+            } > $null
+
+            $wc.DownloadFileAsync($uri, $of)
+            # ps script runs probably in one thread only (event is reised in same thread - blocking problems)
+            # $global:are.WaitOne() not work
+            while(!$global:are.WaitOne(500)) {}
+            Remove-Variable -Name lastpercentage,are,file,path,of -ErrorAction SilentlyContinue
+        }
+
+        if(-not (Test-Path $psexec)){
+            Download-PsExec
+        }
+
+        if(-not (Test-Path $psexec)){
+            throw "PsExec.exe is required for this script to run"
+            return
+        }
+
+
+        Write-Verbose "Building local psexec arguments and remote powershell command"
+        ## Prepare the command line for PsExec. We use the XML output encoding so
+        ## that PowerShell can convert the output back into structured objects.
+        ## PowerShell expects that you pass it some input when being run by PsExec
+        ## this way, so the 'echo .' statement satisfies that appetite.
+        $commandLine = "echo . | powershell -Output XML -WindowStyle Hidden -ExecutionPolicy Unrestricted "
+
+        Write-Verbose "Scriptblock length $($ScriptBlock.length)"
+        $firstargs = New-Object System.Collections.Generic.List[string]
+        $lastargs = New-Object System.Collections.Generic.List[string]
+        $runspacelist = New-Object System.Collections.Generic.List[object]
+
+        '-acceptEula','-nobanner' | ForEach-Object{
+            $firstargs.Add($_)
+        }
+
+        if($noProfile){
+            if($Credential){
+                $firstargs.Add('-e')
             }
-        } > $null
 
-        Register-ObjectEvent -InputObject $wc -EventName DownloadFileCompleted -Action {
-            $global:are.Set()
-        } > $null
+            $commandLine += "-NoProfile "
+        }
 
-        $wc.DownloadFileAsync($uri, $of)
-        # ps script runs probably in one thread only (event is reised in same thread - blocking problems)
-        # $global:are.WaitOne() not work
-        while(!$global:are.WaitOne(500)) {}
-        Remove-Variable -Name lastpercentage,are,file,path,of -ErrorAction SilentlyContinue
+        if($System){
+            $firstargs.Add('-s')
+        }
+
+        if($Credential){
+            # This lets users pass either a username, or full credential to our
+            # credential parameter
+            $credential = Get-Credential $credential
+            $username = $Credential.Username
+            $password = $Credential.GetNetworkCredential().Password
+
+            '-i','-u',$username,'-p',$password | ForEach-Object {
+                $lastargs.Add($_)
+            }
+        }
+        
+        ## Convert the command into an encoded command for PowerShell
+        $commandBytes = [System.Text.Encoding]::Unicode.GetBytes($scriptblock)
+        $encodedCommand = [Convert]::ToBase64String($commandBytes)
+        Write-Verbose "Encodedcommand length $($Encodedcommand.length)"
+
+        if($encodedCommand.length -gt 7800){
+            Write-Verbose "Encoded scriptblock length exceeds limit for powershell"
+            $commandLine += "-File c:\users\public\remoteexpression.ps1"
+        }
+        else{
+            Write-Verbose "Encoded scriptblock length below limit for powershell"
+            $commandLine += "-EncodedCommand $encodedCommand"
+        }
+
+        'powershell.exe',$commandLine | ForEach-Object{
+            $lastargs.Add($_)
+        }
+        
+        $InitialSessionState = [System.Management.Automation.Runspaces.InitialSessionState]::CreateDefault()
+        $InitialSessionState.Variables.Add([System.Management.Automation.Runspaces.SessionStateVariableEntry]::new('psexec',$psexec,'Path to psexec.exe'))
+        $InitialSessionState.Variables.Add([System.Management.Automation.Runspaces.SessionStateVariableEntry]::new('firstargs',$firstargs,'First arguments for psexec'))
+        $InitialSessionState.Variables.Add([System.Management.Automation.Runspaces.SessionStateVariableEntry]::new('lastargs',$lastargs,'Last arguments for psexec'))
+        $InitialSessionState.Variables.Add([System.Management.Automation.Runspaces.SessionStateVariableEntry]::new('scriptblock',$scriptblock,'Script to run against remote system'))
+        $pool = [RunspaceFactory]::CreateRunspacePool(1, $ThrottleLimit,$InitialSessionState,$Host)
+        $pool.ApartmentState = "MTA"
+        $pool.Open()
+
+        $host.UI.RawUI.WindowTitle = 'Invoke-RemoteExpression'
+        
+        $script = {
+            Param(
+                $computer,
+                $verbose
+            )
+
+            $errorfile = New-TemporaryFile
+
+            if($lastargs -match '-File'){
+                $remotefile = "\\$computer\c$\users\public\remoteexpression.ps1"
+
+                Write-Verbose "Creating powershell script $remotefile for psexec" -Verbose:$verbose
+
+                try{
+                    $scriptblock | Set-Content -LiteralPath $remotefile
+                }
+                catch{
+                    Write-Warning "An error occurred writing to $remotefile"
+                    Write-Warning "Please verify $remotefile is accessible or provide a shorter script"
+                    continue
+                }
+            }
+
+            $psexecargs = $firstargs + "\\$Computer" + $lastargs
+        
+            Write-Verbose "Execute psexec command against \\$computer" -Verbose:$verbose
+            
+            # capture to a variable so powershell deserializes the return data
+            $output = try{
+                &$psexec @psexecargs 2>$errorfile
+            }
+            catch{
+                $error[0] | Set-Content $errorfile
+            }
+
+            if(($output | Select-Object -First 1) -is [string]){
+                [PSCustomObject]@{
+                    ComputerName   = $Computer
+                    Output         = $output
+                }
+            }
+            else{
+                $output | Add-Member -MemberType NoteProperty -Name ComputerName -Value $Computer -Force -PassThru
+            }
+
+            <#
+            if($erroroutput -Match 'PsExec.exe : powershell.exe exited with error code (?<ErrorCode>\d*)' -and $matches.ErrorCode -ne 0){
+                Write-Warning "An error occurred running PSExec"
+                Write-Warning $error[0].Exception.Message
+            }
+            #>
+
+            if($LASTEXITCODE -ne 0){
+                
+                $erroroutput = Get-Content -LiteralPath $errorfile.FullName | Where-Object {$_}
+
+                $erroroutput | ForEach-Object {
+                    if($_ -notmatch "Cannot process the XML from the 'Error' stream of|connecting to"){
+                        Write-Warning " [$computer]  $_"
+                    }
+                }
+            }
+            
+            Remove-Item -LiteralPath $errorfile.FullName -ErrorAction SilentlyContinue
+
+            if($remotefile){
+                Write-Verbose "Deleting script $remotefile" -Verbose:$verbose
+                Remove-Item -LiteralPath $remotefile -ErrorAction SilentlyContinue
+            }
+        }
+
+        $psexecargs = $firstargs + '\\$ComputerName' + $lastargs
+
+        Write-Verbose "Psexec argument count: $($psexecargs.Count)"
+
+        $psexecargs | ForEach-Object {
+            if($maskpassword -eq $true){
+                Write-Verbose "Argument: $('*' * (Get-Random (9..17)))"
+                $maskpassword = $false
+            }
+            else{
+                if($_ -eq '-p'){
+                    $maskpassword = $true
+                }
+                Write-Verbose "Argument: $_"
+            }
+        }
     }
-    if(!(Test-Path $psexec)){Download-PsExec}
-    if(!(Test-Path $psexec)){throw "PsExec.exe is required for this script to run"; exit}
+    
+    process{
+        try{
+            foreach($computer in $ComputerName){
+                Write-Verbose "Creating powershell runspace for computer $computer"
 
-    Push-Location $env:temp
-    ## Prepare the computername for PSExec
-    if($ComputerName -notmatch '^\\')
-    {
-        $ComputerName = "\\$ComputerName"
+                $runspace = [PowerShell]::Create()
+                $null = $runspace.AddScript($script)
+                $null = $runspace.AddArgument($computer)
+                $null = $runspace.AddArgument($PSBoundParameters.ContainsKey('Verbose'))
+                $runspace.RunspacePool = $pool
+                $runspacelist.Add([PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke()})
+            }
+        }
+        catch{
+            Write-Warning $_.Exception.Message
+        }
     }
 
-    ## Prepare the command line for PsExec. We use the XML output encoding so
-    ## that PowerShell can convert the output back into structured objects.
-    ## PowerShell expects that you pass it some input when being run by PsExec
-    ## this way, so the 'echo .' statement satisfies that appetite.
-    $commandLine = "echo . | powershell -Output XML "
+    end{        
+        try{
+            while($runspacelist[0]){
+                if($runspacelist[0].Status.IsCompleted){
+                    $runspacelist[0].Pipe.EndInvoke($runspacelist[0].Status)
+                    $runspacelist[0].Pipe.Dispose()
+                    $runspacelist.RemoveAt(0)
+                }
 
-    if($noProfile)
-    {
-        $commandLine += "-NoProfile "
+                Start-Sleep -Milliseconds 200
+            }
+        }
+        finally{
+            $runspace.RunspacePool.Close()
+            $runspace.RunspacePool.Dispose()
+        }
+
+        $host.UI.RawUI.WindowTitle = $title
+        
     }
-    ## Convert the command into an encoded command for PowerShell
-    $commandBytes = [System.Text.Encoding]::Unicode.GetBytes($scriptblock)
-    $encodedCommand = [Convert]::ToBase64String($commandBytes)
-    $commandLine += "-EncodedCommand $encodedCommand"
-
-    ## Collect the output and error output
-    $errorOutput = [IO.Path]::GetTempFileName()
-
-    if($Credential)
-    {
-        ## This lets users pass either a username, or full credential to our
-        ## credential parameter
-        $credential = Get-Credential $credential
-        $networkCredential = $credential.GetNetworkCredential()
-        $username = $networkCredential.Username
-        $password = $networkCredential.Password
-
-        $output = .\psexec.exe $ComputerName /user $username /password $password `
-            /accepteula cmd /c $commandLine 2>$errorOutput
-    }
-    else
-    {
-        $output = .\psexec.exe /acceptEula $computername cmd /c $commandLine 2>$errorOutput
-    }
-    ## Check for any errors
-    $errorContent = Get-Content $errorOutput
-    Remove-Item $errorOutput
-
-    if($lastExitCode -ne 0)
-    {
-        $OFS = "`n"
-        $errorMessage = "Could not execute remote expression. "
-        $errorMessage += "Ensure that your account has administrative " +
-            "privileges on the target machine.`n"
-        $errorMessage += ($errorContent -match "psexec.exe :")
-
-        Write-Error $errorMessage
-    }
-
-    ## Return the output to the user
-    $output | select -Skip 6 | out-file .\clixml.xml -Append -Encoding ascii
-    $outobj = Import-Clixml .\clixml.xml
-    Remove-Item .\clixml.xml
-    Pop-Location
-    $outobj
-    $outobj = $null
+    
 }
