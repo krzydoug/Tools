35af91eddb61dc72295213bc6025748fb8388919
Doug Maurer <dmaurer80@yahoo.com>
2022-09-06 01:54:19 -0500

    Update Get-BCDEdit.ps1

--- Get-BCDEdit.ps1
+++ Get-BCDEdit.ps1
@@ -1,32 +1,55 @@
-#requires -RunAsAdministrator
+function Get-BCDEdit {
+    [cmdletbinding()]
+    Param(
+        [parameter()]
+        [ValidateSet('Enum','V')]
+        [string]$Output = 'Enum',
 
-$ErrorActionPreference = 'Stop'
+        [parameter()]
+        [switch]$Current
+    )
 
-try{
-    $result = bcdedit /V | where {$_ -match "^[^-|^ ]"}
+    $ErrorActionPreference = 'Stop'
+
+    try{
+        $result = bcdedit /$Output | where {$_ -match "^[^-|^ ]"}
     
-    if(!$?){
-        Write-Warning "BCD command failed. Are you running as admin?"
-        break
-    }
+        if(!$?){
+            Write-Warning "BCD command failed. Are you running as admin?"
+            break
+        }
 
-    $result | ForEach-Object -Begin {$props = $null} -Process {
-        $key,$value = $_.Split(' ',2).trim()
-        if($key -eq 'Windows')
-        {
-            if($props.keys.Count -gt 0)
+        $null = $result | ForEach-Object -Begin {$props = $null} -Process {
+            $key,$value = $_.Split(' ',2).trim()
+            if($key -eq 'Windows')
             {
-                [pscustomobject]$props
+                if($props.keys.Count -gt 0)
+                {
+                    [pscustomobject]$props
+                }
+                $props = [ordered]@{
+                    "BCD Entry" = $value
+                }
             }
-            $props = [ordered]@{}
-            $props.Add("BCD Entry",$value)
+            else
+            {
+                $props.Add($key,$value)
+            }
+        } -End {[pscustomobject]$props} -OutVariable bcd
+
+        if($Current){
+            if($Output -eq 'v'){
+                $bcd | Where-Object recoverysequence -eq (Get-BCDEdit -Output Enum | Where-Object identifier -match 'current').recoverysequence
+            }
+            else{
+                $bcd | Where-Object identifier -match 'current'
+            }
         }
-        else
-        {
-            $props.Add($key,$value)
+        else{
+            $bcd
         }
-    } -End {[pscustomobject]$props} -OutVariable bcd
-}
-catch{
-    Write-Warning "Error getting BCD info: $($result)"
+    }
+    catch{
+        Write-Warning "Error getting BCD info: $($result)"
+    }
 }
