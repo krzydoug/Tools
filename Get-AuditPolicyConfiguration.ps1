97c1f8ab02ceec451732319862d2bad463738da8
Doug Maurer <dmaurer80@yahoo.com>
2022-11-04 14:04:16 -0500

    Create Get-AuditPolicyConfiguration.ps1

---
+++ Get-AuditPolicyConfiguration.ps1
@@ -0,0 +1,29 @@
+Function Get-AuditPolicyConfiguration {
+    [cmdletbinding()]
+    Param(
+        [parameter()]
+        [ValidateSet('Text', 'Json', 'PSCustomObject')]
+        $OutputMode = 'PSCustomObject'
+    )
+
+    $auditconfig = auditpol /get /category:* | Select-Object -Skip 3
+
+    $ht = [ordered]@{}
+
+    switch -Regex ($auditconfig) {
+        '^(\w+)' {
+            $current = $Matches.1
+            $ht.$current = [ordered]@{}
+        }
+
+        '^\s+(.+?)\s{3,}(.+)' {
+            $ht."$($current)"."$($matches.1)" = $Matches.2
+        }
+    }
+
+    switch ($OutputMode){
+        Text {$auditconfig}
+        Json {$ht | ConvertTo-Json}
+        default {[PSCustomObject]$ht}
+    }
+}
