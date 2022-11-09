81ae903f58e9b188343def1b121b8e6dd69a322f
Doug Maurer <dmaurer80@yahoo.com>
2022-11-09 16:14:23 -0600

    Update Get-SslCertificate.ps1

--- Get-SslCertificate.ps1
+++ Get-SslCertificate.ps1
@@ -7,37 +7,68 @@ function Get-SslCertificate {
     .EXAMPLE
         PS C:\> Get-SslCertificate -Url https://www.microsoft.com | Select-Object -Property *
         
-        URL            : https://www.microsoft.com
-        Name           : C=US, S=WA, L=Redmond, O=Microsoft Corporation, CN=www.microsoft.com
-        Subject        : CN=www.microsoft.com, O=Microsoft Corporation, L=Redmond, S=WA, C=US
-        Issuer         : C=US, O=Microsoft Corporation, CN=Microsoft RSA TLS CA 01
-        ValidFrom      : 7/8/2022 1:22:47 PM
-        Expiration     : 7/8/2023 1:22:47 PM
-        DaysRemaining  : 308
-        Thumbprint     : 7625E4F156DB2797A134EC418359F6D9FEDDB925
-        RawCertificate : System.Security.Cryptography.X509Certificates.X509Certificate
-    .EXAMPLE
-        PS C:\> 'msn.com','google.com' | Get-SslCertificate
-        
-        WARNING: The https://google.com certificate expires in 65 days
-        
-        Url                Subject         Expiration            Issuer
-        ---                -------         ----------            ------
-        https://msn.com    CN=*.msn.com    9/28/2022 12:11:31 AM C=US, O=Microsoft Corporation, CN=Microsoft RSA TLS CA 02
-        https://google.com CN=*.google.com 11/7/2022 2:17:54 AM  C=US, O=Google Trust Services LLC, CN=GTS CA 1C3
+        Url                : https://www.microsoft.com
+        DaysRemaining      : 324
+        Archived           : False
+        Extensions         : {System.Security.Cryptography.Oid, System.Security.Cryptography.Oid, System.Security.Cryptography.Oid, System.Security.Cryptography.Oid...}
+        FriendlyName       : 
+        IssuerName         : System.Security.Cryptography.X509Certificates.X500DistinguishedName
+        NotAfter           : 9/29/2023 6:23:11 PM
+        NotBefore          : 10/4/2022 6:23:11 PM
+        HasPrivateKey      : False
+        PrivateKey         : 
+        PublicKey          : System.Security.Cryptography.X509Certificates.PublicKey
+        RawData            : {48, 130, 8, 214...}
+        SerialNumber       : 330059F8B6DA8689706FFA1BD900000059F8B6
+        SubjectName        : System.Security.Cryptography.X509Certificates.X500DistinguishedName
+        SignatureAlgorithm : System.Security.Cryptography.Oid
+        Thumbprint         : 2D6E2AE5B36F22076A197D50009DEE66396AA99C
+        Version            : 3
+        Handle             : 2160281246848
+        Issuer             : CN=Microsoft Azure TLS Issuing CA 06, O=Microsoft Corporation, C=US
+        Subject            : CN=www.microsoft.com, O=Microsoft Corporation, L=Redmond, S=WA, C=US
     .EXAMPLE
         PS C:\> Get-SslCertificate bing.com,powershell.org,amazon.com | Select-Object -ExpandProperty RawCertificate
         
-               Handle Issuer                                                    Subject                                                                            
-               ------ ------                                                    -------                                                                            
-        1765019620096 CN=Microsoft RSA TLS CA 01, O=Microsoft Corporation, C=US CN=www.bing.com                                                                    
-        1765019619200 CN=Cloudflare Inc ECC CA-3, O="Cloudflare, Inc.", C=US    CN=sni.cloudflaressl.com, O="Cloudflare, Inc.", L=San Francisco, S=California, C=US
-        1765019624704 CN=DigiCert Global CA G2, O=DigiCert Inc, C=US            CN=*.peg.a2z.com 
+        Url                    Subject                                                                             NotAfter             Issuer                                       
+        ---                    -------                                                                             --------             ------                                       
+        https://bing.com       CN=www.bing.com                                                                     3/2/2023 7:06:28 PM  CN=Microsoft RSA TLS CA 02, O=Microsoft Co...
+        https://powershell.org CN=sni.cloudflaressl.com, O="Cloudflare, Inc.", L=San Francisco, S=California, C=US 7/10/2023 6:59:59 PM CN=Cloudflare Inc ECC CA-3, O="Cloudflare,...
+        https://amazon.com     CN=*.peg.a2z.com                                                                    10/18/2023 6:59:5... CN=DigiCert Global CA G2, O=DigiCert Inc, ...
+    .EXAMPLE
+        PS C:\> 'msn.com','google.com' | Get-SslCertificate
+        
+        WARNING: The https://google.com certificate expires in 68 days
 
+        Url                Subject                                                      NotAfter              Issuer                                                             
+        ---                -------                                                      --------              ------                                                             
+        https://msn.com    CN=*.msn.com, O=Microsoft Corporation, L=Redmond, S=WA, C=US 9/9/2023 12:17:40 PM  CN=Microsoft Azure TLS Issuing CA 01, O=Microsoft Corporation, C=US
+        https://google.com CN=*.google.com                                              1/17/2023 12:13:28 PM CN=GTS CA 1C3, O=Google Trust Services LLC, C=US                                                                        10/18/2023 6:59:5... CN=DigiCert Global CA G2, O=DigiCert Inc, ...
+    .EXAMPLE
+        PS C:\> $cert = 'godaddy.com' | Get-SslCertificate
+        PS C:\> $cert.DnsNameList
+
+        Punycode      Unicode      
+        --------      -------      
+        *.godaddy.com *.godaddy.com
+        godaddy.com   godaddy.com  
+    .EXAMPLE
+        PS C:\> 'cnn.com' | Get-SslCertificate | Get-Member -Static
+
+           TypeName: System.Security.Cryptography.X509Certificates.X509Certificate2
+
+        Name                 MemberType Definition                                                                                                                                   
+        ----                 ---------- ----------                                                                                                                                   
+        CreateFromCertFile   Method     static X509Certificate CreateFromCertFile(string filename)                                                                                   
+        CreateFromSignedFile Method     static X509Certificate CreateFromSignedFile(string filename)                                                                                 
+        Equals               Method     static bool Equals(System.Object objA, System.Object objB)                                                                                   
+        GetCertContentType   Method     static System.Security.Cryptography.X509Certificates.X509ContentType GetCertContentType(byte[] rawData), static System.Security.Cryptograp...
+        new                  Method     System.Security.Cryptography.X509Certificates.X509Certificate2 new(), System.Security.Cryptography.X509Certificates.X509Certificate2 new(b...
+        ReferenceEquals      Method     static bool ReferenceEquals(System.Object objA, System.Object objB)   
     .INPUTS
         String
     .OUTPUTS
-        PSCustomObject
+        System.Security.Cryptography.X509Certificates.X509Certificate2+CustomFormat
     .NOTES
         Used for monitoring certificates to prevent expiration
     #>
@@ -53,10 +84,9 @@ function Get-SslCertificate {
     )
     
     begin {
-        $TypeName = 'SCS.Certificate'
-        $defaultDisplaySet = 'Url', 'Subject', 'Expiration', 'Issuer'
-        $defaultDisplayPropertySet = New-Object System.Management.Automation.PSPropertySet('DefaultDisplayPropertySet',[string[]]$defaultDisplaySet)
-        $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($defaultDisplayPropertySet)
+        $TypeName = 'System.Security.Cryptography.X509Certificates.X509Certificate2+CustomFormat'
+        $defaultDisplaySet = 'Url', 'Subject', 'NotAfter', 'Issuer'
+        Update-TypeData -TypeName $TypeName -DefaultDisplayPropertySet $defaultDisplaySet -Force
         
         $script = {
             Param($site,$timeout)
@@ -91,43 +121,20 @@ function Get-SslCertificate {
                 Write-Warning "URL check error $($site): $($_.exception.message)"
                 continue
             }
-        
-            $certThumbprint = $req.ServicePoint.Certificate.GetCertHashString()
-        
-            $effectiveDate = $req.ServicePoint.Certificate.GetEffectiveDateString()
 
-            $certEffectiveDate = [datetime]::ParseExact(($effectiveDate -replace ' (PM|AM)'), "M/d/yyyy H:mm:ss", $null)
-        
-            if($effectiveDate -match 'PM$'){
-                $certEffectiveDate = $certEffectiveDate.AddHours(12)
-            }
-        
-            $expDate = $req.ServicePoint.Certificate.GetExpirationDateString()
-
-            $certExpDate = [datetime]::ParseExact(($expDate -replace ' (PM|AM)'), "M/d/yyyy H:mm:ss", $null)
-        
-            if($expDate -match 'PM$'){
-                $certExpDate = $certExpDate.AddHours(12)
-            }
-        
-            [int]$certExpiresIn = ($certExpDate - (get-date)).Days
+            [int]$certExpiresIn = ([datetime]$req.ServicePoint.Certificate.GetExpirationDateString() - (get-date)).Days
         
             if ($certExpiresIn -lt $minCertAge){
                 Write-Warning "The $site certificate expires in $certExpiresIn days"
             }
 
-            [PSCustomObject]@{
-                URL            = $site
-                Name           = $req.ServicePoint.Certificate.GetName()
-                Subject        = $req.ServicePoint.Certificate.subject
-                Issuer         = $req.ServicePoint.Certificate.GetIssuerName()
-                ValidFrom      = $certEffectiveDate
-                Expiration     = $certExpDate
-                DaysRemaining  = $certExpiresIn
-                Thumbprint     = $certThumbprint
-                RawCertificate = $req.ServicePoint.Certificate
-            } | Add-Member MemberSet PSStandardMembers $PSStandardMembers -PassThru
-
+            $obj = New-Object system.security.cryptography.x509certificates.x509certificate2($req.ServicePoint.Certificate)
+            $obj.psobject.TypeNames.Insert(0,$TypeName)
+            $null = $obj.psobject.TypeNames.Remove('System.Security.Cryptography.X509Certificates.X509Certificate2')
+            $obj | Add-Member -MemberType ScriptProperty -Name DnsNameList -Value {(new-object Microsoft.Powershell.Commands.DnsNameProperty -argumentlist $this).DnsNameList}
+            $obj | Add-Member -MemberType ScriptProperty -Name EnhancedKeyUsageList -Value {(new-object Microsoft.Powershell.Commands.EnhancedKeyUsageProperty -argumentlist $this).EnhancedKeyUsageList}
+            $obj | Add-Member -NotePropertyName Url -NotePropertyValue $site
+            $obj | Add-Member -NotePropertyName DaysRemaining -NotePropertyValue $certExpiresIn -PassThru
         }
     }
     
@@ -142,7 +149,4 @@ function Get-SslCertificate {
         }
     }
     
-    end {
-        
-    }
 }
