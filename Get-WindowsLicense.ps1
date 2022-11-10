88b757ab54450d642d3482cf77ec22cf42ea3767
Doug Maurer <dmaurer80@yahoo.com>
2022-11-10 10:35:57 -0600

    Update Get-WindowsLicense.ps1

--- Get-WindowsLicense.ps1
+++ Get-WindowsLicense.ps1
@@ -1,14 +1,10 @@
-function Get-WindowsLicense {
+ function Get-WindowsLicense {
     [cmdletbinding()]
     Param()
         
-    $ht = [ordered]@{}
-
     $Slmgr = cscript.exe C:\Windows\system32\slmgr.vbs /dlv
 
-    $Slmgr -match ':' -replace '(?<=^[\s\w]+?):','=' | ForEach-Object {
-        $ht += $_ | ConvertFrom-StringData
-    }
-
-    [pscustomobject]$ht
+    $obj = $Slmgr -match ':' -replace '(?<=^[\s\w]+?):','=' -join "`n" | ConvertFrom-StringData
+    
+    [pscustomobject]$obj
 }
