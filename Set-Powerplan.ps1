d94e7a129ef6818a0adbdc2f42fb7681e84dbb88
Doug Maurer <dmaurer80@yahoo.com>
2023-07-03 12:38:08 -0500

    Update Set-Powerplan.ps1
    Fix parameterset

--- Set-Powerplan.ps1
+++ Set-Powerplan.ps1
@@ -1,11 +1,11 @@
 function Set-PowerPlan{
     [cmdletbinding()]
     Param(
-        [parameter()]
+        [parameter(Mandatory=$true,ParameterSetName='Guid')]
         [ValidateSet("381b4222-f694-41f0-9685-ff5bb260df2e","8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c","a1841308-3541-4fab-bc81-f71556f20b4a")]
         [System.Guid]$Guid,
  
-        [parameter(Mandatory=$true,HelpMessage="You must enter a plan name. Possible values are High Performance, Balanced, or Power Saver")]
+        [parameter(Mandatory=$true,ParameterSetName='Name',HelpMessage="You must enter a plan name. Possible values are High Performance, Balanced, or Power Saver")]
         [ValidateSet("Balanced","High Performance","Power Saver")]
         [System.String]$Name
  
