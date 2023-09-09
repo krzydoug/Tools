03957f76d5b20cb7317c8fdf546b69404ccff4d0
Doug Maurer <dmaurer80@yahoo.com>
2023-09-09 10:21:34 -0500

    Update Get-Netstat.ps1
    Added linux support for IncludeProcessDetails

--- Get-Netstat.ps1
+++ Get-Netstat.ps1
@@ -8,8 +8,8 @@ Function Get-Netstat {
     # https://www.reddit.com/r/PowerShell/comments/khiwjo/parsing_netstat_ano/
 
     $sb = {
-        param($proto,$pcid,$state)
-        $localaddr,$localport,$remoteaddr,$remoteport = $_[1..2] |
+        param($proto,$remote,$local,$pcid,$state)
+        $localaddr,$localport,$remoteaddr,$remoteport = $remote,$local |
             Foreach-Object {-split ($_ -replace '(^.+):(.+$)','$1 $2')}
             
         [PSCustomObject]@{
@@ -22,6 +22,12 @@ Function Get-Netstat {
             State         = $state
         }
     }
+
+    $selectprops = '*',
+                   @{n='FilePath';e={$process.Path}},
+                   @{n='Owner';e={$owner}},
+                   @{n='StartTime';e={$StartTime}},
+                   @{n='CommandLine';e={$process.CommandLine}}
 
     if($IsLinux){
         $netstat = which netstat
@@ -36,57 +42,63 @@ Function Get-Netstat {
         $output = if($sudo){
             & $sudo $netstat -tunpl
             & $sudo $netstat -tunp
-        }
-        else{
+        }else{
             & $netstat -tunpl
             & $netstat -tunp
         }
 
-        switch -Regex ($output){
+        $output = switch -Regex ($output){
             '(TCP.+)\s(?<PID>\d+/.+?)$' {
                 $processid,$program = $matches.PID -split '/'
                 , -split $matches.1 | ForEach-Object {
-                    $localaddr,$localport,$remoteaddr,$remoteport = $_[3..4] -split ':(?=[^:]+$)'
-
-                    [PSCustomObject]@{
-                        Protocol      = $_[0]
-                        LocalAddress  = $localaddr
-                        LocalPort     = $localport
-                        RemoteAddress = $remoteaddr
-                        RemotePort    = $remoteport
-                        ProcessID     = $processid
-                        State         = $_[5]
-                    }
+                    $sb.Invoke($($_[0,3,4] + $processid + $_[5]))
                 }
             }
+        }
+
+        if($IncludeProcessDetails){
+            $output | ForEach-Object {
+                $process = Get-Process -PID $_.ProcessID
+                $starttime = $process.StartTime
+
+                $owner = (ps -o user,pid | ForEach-Object{
+                    , -split $_ | ForEach-Object{
+                        [PSCustomObject]@{User=$_[0];PID=$_[1]}
+                    } }|where pid -eq  $_.ProcessID).user
+
+                $_ | Select-Object $selectprops
+            }
+        }
+        else{
+            $output
         }
     }
     else{
         $output = switch -Regex (netstat -ano){
             'TCP' {
                 , -split $_ | ForEach-Object {
-                    $sb.Invoke($($_[0,4,3]))
+                    $sb.Invoke($($_[0,1,2,4,3]))
                 }
             }
             'UDP' {
                 , -split $_ | ForEach-Object {
-                    $sb.Invoke($_[0],$_[3],'STATELESS')
+                    $sb.Invoke($_[0..3] + 'STATELESS')
                 }
             }
         }
 
         if($IncludeProcessDetails){
-            $process = Get-CimInstance -ClassName Win32_Process | Group-Object -Property ProcessID -AsHashTable -AsString
+            $processlist = Get-CimInstance -ClassName Win32_Process | Group-Object -Property ProcessID -AsHashTable -AsString
 
             $output | ForEach-Object {
-                $owner = $process[$_.processid] | Invoke-CimMethod -MethodName GetOwner |ForEach-Object{
+                $process = $processlist[$_.processid]
+                $starttime = $process.CreationDate
+
+                $owner = $process | Invoke-CimMethod -MethodName GetOwner |ForEach-Object{
                     if($_.returnvalue -eq 0){$_.Domain,$_.User -join '\'}
                 }
 
-                $_ | Select-Object *,@{n='FilePath';e={$process.$($_.processid).Path}},
-                                     @{n='Owner';e={$owner}},
-                                     @{n='StartTime';e={$process.$($_.processid).CreationDate}},
-                                     @{n='CommandLine';e={$process.$($_.processid).CommandLine}}
+                $_ | Select-Object $selectprops
             }
         }
         else{
